cd89375f772faa36709e220f615bbfb7a60c79bb
emjayMJkim <minjeong100100@gmail.com>
2023-04-11 00:50:56 +0900

    mainactivity

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -2,10 +2,51 @@ package org.android.go.sopt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fcv_main, HomeFragment()).commit()
+        }
+
+        binding.bncMain.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_home -> {
+                        changeFragment(HomeFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.menu_search -> {
+                        changeFragment(SearchFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.menu_photo -> {
+                        changeFragment(GalleryFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                    else -> {
+
+                    }
+                },
+                false
+            )
+        }
     }
+    private fun changeFragment(fragment: Unit): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commit()
+    }
+
+
 }
