41327af880d1e7d3a52b5a5fca67756d2a0db686
emjayMJkim <minjeong030420@gmail.com>
2023-06-30 13:13:16 +0900

    정규식 수정 & const 처리

---
+++ app/src/main/java/org/android/go/sopt/data/datasource/base/Const.kt
@@ -0,0 +1,4 @@
+package org.android.go.sopt.data.datasource.base
+
+const val ID_REGEX = "^(?=.*[0-9])(?=.*[a-zA-Z]).{6,10}"
+const val PW_REGEX = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#\$%^+\\-=]).{6,10}$"
